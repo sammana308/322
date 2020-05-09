94e2a326d0c7476fa3e5e2986ffafa7eb0f8b0b5
sammana308 <60299991+sammana308@users.noreply.github.com>
2020-05-08 21:44:13 -0700

    posting mole.c file

---
+++ lab6/mole.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+//main function
+int main(int argc, char **argv){
+    FILE * filePointer= fopen("lab6.log", "a+");
+    if (filePointer == NULL){
+        fprintf(stderr, "Error message: No log file created");
+    }
+    if (filePointer != 0){
+        char * mole;
+        if (argc > 1 && strcmp(argv[1], "1") == 0)
+            mole = "Pop mole1";
+        else
+            mole = "Pop mole2";
+        fputs(mole, filePointer);
+        fclose(filePointer);    //gives command
+    }
+    return 0;
+}
